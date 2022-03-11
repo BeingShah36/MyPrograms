f101bdfb5100ab848c1e7b369a8199a3e17c8081
BeingShah36 <shahbazalam.sayyad@gmail.com>
2022-03-11 12:41:10 +0530

    to select multiple quantities from dropdown

---
+++ DropdownLoop.java
@@ -0,0 +1,33 @@
+package intro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DropdownLoop {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+System.setProperty("webdriver.chrome.driver","C:\\personal\\Selenium\\drivers\\chromedriver_win32\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+	driver.manage().window().maximize();	
+	driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+	
+	driver.findElement(By.cssSelector("div.paxinfo")).click();
+	
+	Thread.sleep(2000);
+	for(int i=0;i<4;i++)
+	{
+	driver.findElement(By.id("hrefIncChd")).click();
+	}
+	
+	
+	driver.findElement(By.id("btnclosepaxoption")).click();
+	System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+
+	}
+
+}
