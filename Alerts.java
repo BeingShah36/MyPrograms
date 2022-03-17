dfad71709cdb2eee532b3279910edb0996c11040
BeingShah36 <shahbazalam.sayyad@gmail.com>
2022-03-17 17:11:30 +0530

    Handling popups alerts

---
+++ Alerts.java
@@ -0,0 +1,34 @@
+package intro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alerts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","C:\\personal\\Selenium\\drivers\\chromedriver_win32\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+        driver.manage().window().maximize();
+        
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        
+        driver.findElement(By.id("name")).sendKeys("shah");
+        driver.findElement(By.id("alertbtn")).click();
+        
+        // it will press ok(positive response) in popup
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
+        
+        driver.findElement(By.id("confirmbtn")).click();
+        
+     // it will press cancel(Negative response) in popup
+        driver.switchTo().alert().dismiss();
+ 
+	}
+
+}
