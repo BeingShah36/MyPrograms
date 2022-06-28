aba3dd2aebd85698494ed6b130cd4ffba57c3fc4
BeingShah36 <shahbazalam.sayyad@gmail.com>
2022-06-28 16:31:51 +0530

    Calendar handling

---
+++ Calendars.java
@@ -0,0 +1,46 @@
+package intro;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Calendars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\personal\\Selenium\\drivers\\chromedriver_win32\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.path2usa.com/travel-companions");
+		
+		driver.findElement(By.name("travel_date")).click();
+		// String MonthHandle = driver.findElement(By.cssSelector("table[class=' table-condensed'] th[class='datepicker-switch']")).getText();
+        //System.out.println(Month);
+		//select august. here we have not use findelements because month field is same for all the months. at one place only month is changing
+		// while(!MonthHandle.contains("August 2022"))
+		
+		while(!driver.findElement(By.cssSelector("table[class=' table-condensed'] th[class='datepicker-switch']")).getText().contains("August"))
+		{
+			
+			driver.findElement(By.cssSelector("[class='datepicker-days'] th[class='next']")).click();
+			
+			//driver.findElement(By.cssSelector("table[class=' table-condensed'] th[class='next']")).click(); //to click next button
+		}
+		
+		List<WebElement> Date = driver.findElements(By.className("day"));
+		for (int i=0; i<Date.size(); i++)
+		{
+			String day = Date.get(i).getText();
+			if(day.equalsIgnoreCase("9"))
+			{
+				Date.get(i).click();
+				break;
+			}
+		}
+	}
+
+}
