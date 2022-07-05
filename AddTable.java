aa7fd919c0efc4a8b35f10e7cd867cf460240348
BeingShah36 <shahbazalam.sayyad@gmail.com>
2022-07-05 15:13:51 +0530

    adding table content with parseInt and taking screenshots

---
+++ AddTable.java
@@ -0,0 +1,50 @@
+package intro;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class AddTable {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\personal\\Selenium\\drivers\\chromedriver_win32\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		getScreenshot(driver);
+
+		List<WebElement> Numbers = driver.findElements(By.cssSelector(".table-display td:nth-child(3)"));
+
+		int sum = 0;
+		for (int i = 0; i < Numbers.size(); i++) {
+			int a = Integer.parseInt(Numbers.get(i).getText());
+			sum = sum + a;
+		}
+		System.out.println(sum);
+		int b = Integer.parseInt(driver.findElement(By.cssSelector(".totalAmount")).getText().split(":")[1].trim());
+		System.out.println(b);
+
+		
+	}
+	
+	public static void getScreenshot(WebDriver driver) throws IOException {
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+		FileUtils.copyFile(src, new File("C:\\Users\\tony\\img\\img.png"));
+	}
+
+	
+
+}
